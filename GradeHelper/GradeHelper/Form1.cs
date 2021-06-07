031d3bd1789737732e17c2b5363bf050de294724
blagojpetkov <blagoj5kov1@live.com>
2021-06-07 16:07:51 +0200

    Added GUI main menu

---
+++ GradeHelper/GradeHelper/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //professor
+            Login form = new Login("1234");
+            if(form.ShowDialog()==DialogResult.OK)
+            {
+                MainMenuProfessor form1 = new MainMenuProfessor();
+                form1.ShowDialog();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            pictureBox2_Click(null, null);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            //assistant
+            Login form = new Login("123");
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                MainMenuAssistant form1 = new MainMenuAssistant();
+                form1.ShowDialog();
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            pictureBox4_Click(null, null);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            MainMenuStudent form = new MainMenuStudent();
+            form.ShowDialog();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            pictureBox3_Click(null, null);
+        }
+    }
+}
---
+++ GradeHelper/GradeHelper/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeHelper
+{
+    public partial class Login : Form
+    {
+        public string password { get; set; }
+        public string name { get; set; }
+
+        public Login(string password)
+        {
+            InitializeComponent();
+            this.password = password;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == password)
+            {
+                DialogResult = DialogResult.OK;
+            }
+            errorLabel.Text = "Внесовте погрешна лозинка";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.No;
+        }
+    }
+}
